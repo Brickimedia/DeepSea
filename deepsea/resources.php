796c9dba8effbaca2415732ea24272648a20d6c2
Lewis Cawte <lewis@lewiscawte.me>
2013-11-17 22:27:22 +0000

    More path stuff
    Fix resource pathing.

--- deepsea/resources.php
+++ deepsea/resources.php
@@ -1,6 +1,8 @@
-<?php/*
+<?php
+/*
 $big = "$IP/skins/deepsea/big.css";
-$small = "$IP/skins/deepsea/small.css";
+$small = "$IP/skins/deepsea/small.css";
+
 if($_GET['usemobile'] == 'true'){
 	$mobile = true;
 }
@@ -12,25 +14,27 @@ if($mobile) {
 */
 global $wgUser;
 //$user = $wgUser->getName();
-//$globalcss = "meta.brickimedia.org/index.php/User:$user/global.css";
+//$globalcss = "meta.brickimedia.org/index.php/User:$user/global.css";
+
 $wgResourceModules['skins.deepsea'] = array(
     'styles' => array(
         "$IP/skins/common/commonElements.css" => array( 'media' => 'screen' ),
         "$IP/skins/common/commonContent.css" => array( 'media' => 'screen' ),
         "$IP/skins/common/commonInterface.css" => array( 'media' => 'screen' ),
-        "$IP/skins/deepsea/screen.css" => array( 'media' => 'screen' ),
+        "$IP/skins/DeepSea/deepsea/screen.css" => array( 'media' => 'screen' ),
         //"$flexiblestylesheet" => array( 'media' => 'screen' ),
-        "$IP/skins/deepsea/big.css" => array( 'media' => 'only screen and (min-width: 800px), only screen and (min-device-width: 800px)' ),
-        "$IP/skins/deepsea/small.css" => array( 'media' => 'only screen and (max-width: 800px), only screen and (max-device-width: 800px)' ),
-        "$IP/skins/deepsea/interactive.css" => array( 'media' => 'screen' ),
+        "$IP/skins/DeepSea/deepsea/big.css" => array( 'media' => 'only screen and (min-width: 800px), only screen and (min-device-width: 800px)' ),
+        "$IP/skins/DeepSea/deepsea/small.css" => array( 'media' => 'only screen and (max-width: 800px), only screen and (max-device-width: 800px)' ),
+        "$IP/skins/DeepSea/deepsea/interactive.css" => array( 'media' => 'screen' ),
     	"$IP/skins/common/forums.css" => array( 'media' => 'screen' ),
     	//"$globalcss" => array( 'media' => 'screen' )
-    	"$IP/skins/deepsea/projects/$bmProject.css" => array( 'media' => 'screen' )
+    	"$IP/skins/DeepSea/deepsea/projects/$bmProject.css" => array( 'media' => 'screen' )
     ),
     'scripts' => array(
-    	"$IP/skins/deepsea/deepsea.js",
+    	"$IP/skins/DeepSea/deepsea/deepsea.js",
     	"$IP/skins/common/foes.js"
     ),
     'remoteBasePath' => $GLOBALS['wgStylePath'],
-    'localBasePath' => $GLOBALS['wgStyleDirectory']
+    'localBasePath' => $GLOBALS['wgStyleDirectory']
+
 );
